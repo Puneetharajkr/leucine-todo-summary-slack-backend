81971f0549e4ad3783ab8a945f60b56a285d1c15
Puneetharaj K R <100867687+Puneetharajkr@users.noreply.github.com>
2025-05-24 10:43:15 +0530

    Update summary.js

--- controllers/summary.js
+++ controllers/summary.js
@@ -20,7 +20,7 @@ exports.summarizeTodos = async (req, res) => {
     // Send to Slack
     if (process.env.SLACK_WEBHOOK_URL) {
       try {
-        await axios.post(process.env.SLACK_WEBHOOK_URL, {
+        await axios.post('https://hooks.slack.com/services/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', {
           text: `📝 *Todo Summary*:\n${summary}\n\n*Tasks*:\n${todos.map(t => `• ${t}`).join('\n')}`,
           mrkdwn: true
         });
